0312a2ab1b10bc7045b0952fe5e1de83e5b83026
jaydenhubb <jaydenblakelex@gmail.com>
2022-07-23 20:47:50 +0100

    responsive hamburger menu added

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const hamburger = document.getElementById("hamburger");
 
 const menu = document.getElementById("menu");
 
-const close = document.getElementById('close')
+const close = document.getElementById("close");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
-  hamburger.classList.toggle('hidden')
-  close.classList.toggle('hidden')
+  hamburger.classList.toggle("hidden");
+  close.classList.toggle("hidden");
 });
-console.log('jdrry');
+console.log("jdrry");
 
-
-close.addEventListener('click', () => {
-    close.classList.toggle("hidden");
-    hamburger.classList.toggle("hidden");
-    menu.classList.toggle("hidden");
-})
+close.addEventListener("click", () => {
+  close.classList.toggle("hidden");
+  hamburger.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
+});
